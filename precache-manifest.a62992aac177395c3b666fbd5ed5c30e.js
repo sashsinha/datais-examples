ecee841eda13c8a1df46402c5ab19d16279b5555
Shash Sinha <shash678@outlook.com>
2019-10-09 22:10:06 -0400

    Updates

---
+++ precache-manifest.a62992aac177395c3b666fbd5ed5c30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12803ac22ae4506745cff381d13adf6f",
+    "url": "/datais-examples/index.html"
+  },
+  {
+    "revision": "e42435e4efc4181ae30a",
+    "url": "/datais-examples/static/css/main.5ca8efef.chunk.css"
+  },
+  {
+    "revision": "71b2af7740a85df38f13",
+    "url": "/datais-examples/static/js/2.75530400.chunk.js"
+  },
+  {
+    "revision": "e42435e4efc4181ae30a",
+    "url": "/datais-examples/static/js/main.79d08157.chunk.js"
+  },
+  {
+    "revision": "375919ac0ff535a36c7c",
+    "url": "/datais-examples/static/js/runtime-main.95d4cd1c.js"
+  },
+  {
+    "revision": "288573ce7368760d76be94987830de02",
+    "url": "/datais-examples/static/media/logo.288573ce.svg"
+  }
+]);
